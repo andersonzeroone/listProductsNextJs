d6fc0c8f41d66b619246834d9f4985558137605f
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-25 21:20:38 -0300

    refactoring page productDetails

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,11 @@ import path from 'path';
 function ProductDetailPage(props) {
 
   const { loadedProduct } = props;
+
+
+  if (!loadedProduct) {
+    return <p>Loading....</p>
+  }
   return (
     <>
       <h1>{loadedProduct.title}</h1>
@@ -12,16 +17,23 @@ function ProductDetailPage(props) {
   )
 }
 
+
+async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  fs.readFile(filePath);
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  fs.readFile(filePath);
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find(product => product.id === productId);
 
@@ -35,12 +47,15 @@ export async function getStaticProps(context) {
 
 
 export async function getStaticPaths() {
+
+  const data = await getData();
+
+  const ids = data.products.map(product => product.id);
+
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: 'p1' } },
-      { params: { pid: 'p2' } },
-      { params: { pid: 'p3' } },
-    ],
+    paths: pathWithParams,
     fallback: false
   }
 
